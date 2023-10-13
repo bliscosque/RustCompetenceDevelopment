d051eaa6a88c6c5a14cca3dd55df9b36dd8674b4
Thiago <bliscosque@yahoo.com.br>
2023-10-13 12:41:50 -0600

    option enum

--- 1-intro/8-enum.rs
+++ 1-intro/8-enum.rs
@@ -21,4 +21,19 @@ fn main() {
 
     let msg1:Message=Message::Move{x:1,y:2};
     println!("{:?}",msg1);
+
+    //option enum
+    let five=Some(5);
+    let six=plus_one(five);
+    let none=plus_one(None);
+
+    println!("six: {:?}",six);
+    println!("none: {:?}",none);
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i+1),
+    }
 }
