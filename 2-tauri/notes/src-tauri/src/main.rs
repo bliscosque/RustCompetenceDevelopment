939a74afa00ae605853d5bcf398c8960283fed42
Thiago <bliscosque@yahoo.com.br>
2023-11-08 20:51:33 -0600

    adding sql plugin

--- 2-tauri/notes/src-tauri/src/main.rs
+++ 2-tauri/notes/src-tauri/src/main.rs
@@ -10,6 +10,7 @@ fn convert_markdown(text: &str) -> String {
 
 fn main() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_sql::Builder::default().build())
         .invoke_handler(tauri::generate_handler![convert_markdown])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
--- 2-tauri/notes/src/App.jsx
+++ 2-tauri/notes/src/App.jsx
@@ -1,18 +1,28 @@
+import { useState } from "react";
 import Editor from "./Editor";
 
 
 function App() {
+
+  const [notes,setNotes]=useState([1]);
+
   return (
     <div className="bg-gray-700 h-screen p-2">
       <div className="flex flex-row justify-between items-center">
         <h1 className="text-white font-bold">All notes</h1>
-        <button className="btn btn-sm">Add notes</button>
+        <button className="btn btn-sm" onClick={() => {
+          setNotes([...notes,1]);
+          console.log(notes)
+        }}>Add notes</button>
       </div>
       <input className="my-2 w-full input"></input>
-      <div className="flex flex-row justify-between items-center bg-green-200 border-green-500 my-2">
-        <div className="cursor-pointer w-full h-full"><h2>Note</h2></div>
-        <button className="btn btn-sm">Delete me</button>
-      </div>
+      {
+        notes.map((item) => (
+          <div className="flex flex-row justify-between items-center bg-green-200 border-green-500 my-2">
+            <div className="cursor-pointer w-full h-full"><h2>Note</h2></div>
+            <button className="btn btn-sm">Delete me</button>
+          </div>
+      ))}
     </div>
   )
 }
