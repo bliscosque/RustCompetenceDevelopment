08489f4dfdc2be60b5a9a0f910949a0dc6f8f698
Thiago <bliscosque@yahoo.com.br>
2023-11-25 18:25:39 -0600

    single linked list

---
+++ 5-DS/list/src/lib.rs
@@ -0,0 +1,17 @@
+mod linked_list;
+
+mod tests {
+    use crate::*;
+    #[test]
+    fn linked_list() {
+        //create
+        let mut ll=linked_list::LinkedList::new();
+        assert_eq!(ll.length,0);
+        
+        //insert
+        ll.append("1st".to_string());
+        ll.append("2nd".to_string());
+        ll.append("3rd".to_string());
+        assert_eq!(ll.length,3);
+    }
+}
---
+++ 5-DS/list/src/linked_list.rs
@@ -0,0 +1,46 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
+type Link=Option<Rc<RefCell<Node>>>;
+
+#[derive(Clone)]
+struct  Node {
+    value: String,
+    next: Link, //pointer to next node
+}
+
+//node of List
+impl Node {
+    fn new(value: String) -> Rc<RefCell<Node>> {
+        Rc::new(RefCell::new(Node {
+            value,
+            next:None,
+        }))
+    }
+}
+
+//Single Linked List
+pub struct LinkedList {
+    head: Link,
+    tail: Link,
+    pub length: u64,
+}
+
+impl LinkedList {
+    pub fn new() -> LinkedList {
+        LinkedList {
+            head: None,
+            tail: None,
+            length: 0,
+        }
+    }
+    pub fn append(&mut self, value: String) {
+        let new=Node::new(value);
+        match self.tail.take() {
+            Some(old) => old.borrow_mut().next=Some(new.clone()),
+            None => self.head=Some(new.clone())
+        };
+        self.length+=1;
+        self.tail=Some(new);
+    }
+}
