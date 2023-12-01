320621f351086fc962d7ef8434b091585e9a2381
Thiago <bliscosque@yahoo.com.br>
2023-12-01 08:02:00 -0600

    continue with cpu emulator

--- 4-RustInAction/cpu_emul/src/main.rs
+++ 4-RustInAction/cpu_emul/src/main.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 struct CPU {
     registers: [u8;16], //16 registers
     position_in_memory: usize, //program counter (next instruction)
@@ -24,10 +25,86 @@ impl CPU {
             // 0x73EE = 7(Opcode) | 3 (Reg x) | EE(Arg (kk)) = Add 238 (0xEE) to reg 3
             // 0x1200 = 1(Opcode) | 200 (address) = Jump to memory addr 0x200
             // 0x8231 = 8(Opcode) | 2 (reg x) | 3 (reg y) | 1 (opcode subtype) = bitwise OR with reg x and reg y, stores at reg x
+
+            self.position_in_memory += 2; //advancing...
+
+            match opcode {
+                0x0000 => { return; },
+                0x00E0 => { /* CLEAR SCREEN */ },
+                0x00EE => { self.ret(); },
+                // 0x1000..=0x1FFF => { self.jmp(addr); },
+                0x2000..=0x2FFF => { self.call(addr); }
+                /*
+                0x3000..=0x3FFF => { self.se(x, kk); },
+                0x4000..=0x4FFF => { self.sne(x, kk); },
+                0x5000..=0x5FFF => { self.se(x, y); },
+                0x6000..=0x6FFF => { self.ld(x, kk); },
+                0x7000..=0x7FFF => { self.add(x, kk); },
+                */
+                0x8000..=0x8FFF => {
+                    match op_minor { //last bit
+                        //0 => { self.ld(x, self.registers[y as usize]) },
+                        //1 => { self.or_xy(x, y) },
+                        //2 => { self.and_xy(x, y) },
+                        //3 => { self.xor_xy(x, y) },
+                        4 => { self.add_xy(x, y); },
+                        _ => { todo!("opcode: {:04x}", opcode); },
+                    }
+                },
+                _ => todo!("opcode {:04x}", opcode),
+            }
         }
+    }
+
+    // opcode 2xxx - CALL at addr (xxx)
+    fn call(&mut self, addr: u16) {
+        println!("At call to addr: {:x}",addr);
+        let sp=self.stack_pointer;
+        let stack=&mut self.stack;
+        if sp>=stack.len() {
+            panic!("Stack overflow!")
+        }
+        stack[sp]=self.position_in_memory as u16;
+        self.stack_pointer+=1;
+        self.position_in_memory=addr as usize;
+    }
+
+    // opcode 8xy4 - ADD (reg x=reg x + reg y)
+    fn add_xy(&mut self, x:u8, y:u8) {
+        self.registers[x as usize] += self.registers[y as usize];
+        //TODO: CARRY FLAG
+    }
+
+    // opcode 00EE RET (return from subroutine)
+    fn ret(&mut self) {
+        if self.stack_pointer==0 {
+            panic!("Stack underflow")M
+        }
+        self.stack_pointer-=1;
+        self.position_in_memory = self.stack[self.stack_pointer] as uside;
     }
 }
 
 fn main() {
-    println!("Hello, world!");
+    let mut cpu = CPU {
+        registers: [0;16],
+        memory: [0;4096],
+        position_in_memory: 0,
+        stack: [0;16],
+        stack_pointer: 0,
+    };
+
+    //initializing a few reg with values
+    cpu.registers[0]=5;
+    cpu.registers[1]=10;
+    
+    cpu.memory[0x000] = 0x21; cpu.memory[0x001] = 0x00; // 2=call 0x100
+    cpu.memory[0x002] = 0x21; cpu.memory[0x003] = 0x00; // 2=call 0x100
+
+    //Pos 0x100 of the Call
+    cpu.memory[0x100] = 0x80; cpu.memory[0x101] = 0x14; // ADD reg 0 with reg 1 (registers: [15, 10,...])  reg0=5+10=15
+    cpu.memory[0x102] = 0x80; cpu.memory[0x103] = 0x14; // ADD reg 0 with reg 1 (registers: [15, 10,...])  reg0=15+10=25
+
+    cpu.run();
+    println!("{:?}",cpu.registers);
 }
