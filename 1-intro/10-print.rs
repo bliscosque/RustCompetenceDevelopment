01df4f2ad90f51714fa8fda56b5dc43b28107938
Thiago <bliscosque@yahoo.com.br>
2023-12-29 17:51:37 -0600

    testing with print

---
+++ 1-intro/10-print.rs
@@ -0,0 +1,6 @@
+fn main() {
+    //positional printing
+    println!("This is {1} {0}", "print", "positional");
+    //named printing
+    println!("This is {what} {other}", what="named", other="printing");
+}
