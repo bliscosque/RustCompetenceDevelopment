44934ce826d0c6c1aa7b7c065f9d5d0ec457fcb1
Thiago <bliscosque@yahoo.com.br>
2023-12-29 17:56:15 -0600

    started stack

---
+++ 5-DS/stack/src/stack.rs
@@ -0,0 +1,8 @@
+fn new_stack(max_size: usize) -> Vec<u32> {
+    let vec:Vec<u32> = Vec::with_capacity(max_size);
+    vec
+}
+
+fn push(stack: &mut Vec<u32>, item: u32, max_size: usize) {
+    //TBI
+}
