03de53ba4aae069b43102f975214f9baa0937cf8
Thiago <bliscosque@yahoo.com.br>
2024-02-11 19:39:15 -0600

    finished catdex

--- 7-practical-rust-web-proj/c2/catdex/src/main.rs
+++ 7-practical-rust-web-proj/c2/catdex/src/main.rs
@@ -5,6 +5,7 @@ extern crate serde_derive;
 extern crate diesel;
 
 use std::env;
+use std::collections::HashMap;
 mod models;
 mod schema;
 use self::models::*;
@@ -12,13 +13,16 @@ use self::schema::cats::dsl::*;
 
 // use serde_json::json; //enable json! (macro)
 use actix_files::Files; 
-use actix_web::{web, App, Error, HttpResponse, HttpServer};
+use actix_web::{http, web, App, Error, HttpResponse, HttpServer};
 
 use handlebars::{Handlebars, DirectorySourceOptions};
+
+use awmp::Parts;
 
 use diesel::prelude::*; 
 use diesel::pg::PgConnection; 
 use diesel::r2d2::{self, ConnectionManager};
+use diesel::result::Error as DieselError;
 
 // PgConnection comes from diesel::prelude
 type DbPool = r2d2::Pool<ConnectionManager<PgConnection>>;
@@ -74,6 +78,82 @@ async fn index(hb: web::Data<Handlebars<'_>>, pool: web::Data<DbPool> ) -> Resul
     */
 }
 
+#[derive(Serialize)]
+enum CatTemplateData {
+    Success(models::Cat),
+    Error(String), // You can customize the error type based on your needs
+}
+
+impl From<Result<models::Cat, DieselError>> for CatTemplateData {
+    fn from(result: Result<models::Cat, DieselError>) -> Self {
+        match result {
+            Ok(cat) => CatTemplateData::Success(cat),
+            Err(err) => CatTemplateData::Error(err.to_string()),
+        }
+    }
+}
+
+async fn add(hb: web::Data<Handlebars<'_>>) -> Result<HttpResponse, Error> {
+
+    let body = hb.render("add", &{}).unwrap();
+
+    Ok(HttpResponse::Ok().body(body))
+}
+
+
+async fn add_cat_form(pool: web::Data<DbPool>, mut parts: Parts) -> Result<HttpResponse, Error> {
+
+    //warning: security issues with the way we save the files (can overwrite, can send malicious files, etc)
+    let file_path = 
+        parts.files
+        .take("image")
+        .pop()
+        .and_then(|f| f.persist_in("./static/image").ok())
+        .unwrap_or_default();
+    
+    let text_fields: HashMap<_, _> = parts.texts.as_pairs().into_iter().collect();
+    
+    //get a connection
+    let mut connection = pool.get().expect("Can't get db connection from pool");
+    let new_cat = NewCat {
+        name: text_fields.get("name").unwrap().to_string(),
+        image_path: file_path.to_string_lossy().to_string()
+    };
+
+
+    //insert a row into DB
+    web::block(move || 
+        diesel::insert_into(cats)
+        .values(&new_cat)
+        .execute(&mut connection)
+    )
+    .await
+    .map_err(|_| {
+        HttpResponse::InternalServerError().finish()
+    }).unwrap();
+
+    //return a proper response
+    // 303 See itger -> redirect to new location
+    Ok(HttpResponse::SeeOther().header(http::header::LOCATION, "/").finish())
+}
+
+async fn cat(hb: web::Data<Handlebars<'_>>, pool: web::Data<DbPool>, cat_id: web::Path<i32>) -> Result<HttpResponse, Error> {
+    let mut connection = pool.get().expect("Can't get db connection from pool");
+
+    let cat_data = web::block(move || cats.filter(id.eq(cat_id.into_inner())).first::<Cat>(&mut connection))
+        .await
+        .map_err(|_| {
+            HttpResponse::InternalServerError().finish()
+        });
+
+    let template_data = CatTemplateData::from(cat_data.unwrap());
+
+    let body = hb.render("cat", &template_data).unwrap();
+
+    Ok(HttpResponse::Ok().body(body))
+}
+
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let mut handlebars = Handlebars::new(); //start handlebars template
@@ -91,7 +171,10 @@ async fn main() -> std::io::Result<()> {
 
     println!("Listening on port 8080");
     HttpServer::new(move || {App::new().app_data(handlebars_ref.clone()).app_data(web::Data::new(pool.clone())).service(Files::new("/static", "static").show_files_listing(),)
-        .route("/",web::get().to(index))})
+        .route("/",web::get().to(index))
+        .route("/add",web::get().to(add))
+        .route("/cat/{id}", web::get().to(cat))
+        .route("/add_cat_form", web::post().to(add_cat_form))})
         .bind("127.0.0.1:8080")?
         .run()
         .await
--- 7-practical-rust-web-proj/c2/catdex/src/models.rs
+++ 7-practical-rust-web-proj/c2/catdex/src/models.rs
@@ -1,9 +1,17 @@
 use serde::{Deserialize, Serialize}; 
 use super::schema::cats; 
 
-#[derive(Queryable, Serialize)]
+#[derive(Queryable, Serialize, Deserialize)]
 pub struct Cat { 
     pub id: i32, 
     pub name: String, 
     pub image_path: String 
+}
+
+#[derive(Insertable, Serialize, Deserialize)]
+#[table_name = "cats"] // cats table is already used, so we have to annotate
+pub struct NewCat {
+    // id will be added by the database
+    pub name: String,
+    pub image_path: String
 }
