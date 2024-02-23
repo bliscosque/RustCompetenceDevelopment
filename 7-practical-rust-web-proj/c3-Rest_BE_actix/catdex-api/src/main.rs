ef1be14e400d5d6b7c534bc30b0f0cca899a094d
Thiago <bliscosque@yahoo.com.br>
2024-02-23 15:05:06 -0600

    some more on web

--- 7-practical-rust-web-proj/c3-Rest_BE_actix/catdex-api/src/main.rs
+++ 7-practical-rust-web-proj/c3-Rest_BE_actix/catdex-api/src/main.rs
@@ -19,14 +19,6 @@ use std::env;
 
 type DbPool = r2d2::Pool<ConnectionManager<PgConnection>>;
 
-
-#[derive(Serialize)]
-pub struct Cat {
- pub id: i32,
- pub name: String,
- pub image_path: String,
-}
-
 /*
 async fn cats() -> impl Responder {
     let cats = vec![
@@ -58,19 +50,46 @@ async fn cats_endpoint(pool: web::Data<DbPool>) -> Result<HttpResponse, Error> {
 }
 
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
+fn setup_database() -> DbPool {
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     let manager = ConnectionManager::<PgConnection>::new(&database_url);
-    let pool = r2d2::Pool::builder().build(manager).expect("Failed to create DB connection pool.");
+    r2d2::Pool::builder().build(manager).expect("Failed to create DB connection pool.")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let pool = setup_database();
 
 
     println!("Listening on port 8080");
     HttpServer::new(move || {App::new()
+            .data(pool.clone())
             .service(web::scope("/api").route("/cats", web::get().to(cats_endpoint)),)
             .service(Files::new("/", "static").show_files_listing(),)
         })
     .bind("127.0.0.1:8080")?
     .run()
     .await
+}
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use actix_web::{test, App};
+
+    #[actix_rt::test]
+    async fn test_cats_endpoint_get() {
+        let pool = setup_database();
+            let mut app = test::init_service(
+            App::new()
+                .data(pool.clone())
+                //.configure(api_config)
+                .route("/api/cats", web::get().to(cats_endpoint))
+        ).await;
+        let req = test::TestRequest::get().uri("/api/cats").to_request();
+        let resp = test::call_service(&mut app, req).await;
+        assert!(resp.status().is_success()); // response code 200-299
+    }
 }
